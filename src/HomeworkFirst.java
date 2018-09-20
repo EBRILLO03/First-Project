8b45e1ed94a943632e7e3979b6cafd06a9112555
EBRILLO03 <42975610+EBRILLO03@users.noreply.github.com>
2018-09-19 20:13:31 -0400

    Add files via upload

---
+++ src/HomeworkFirst.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Ethan Brilliant
+ * First Program
+ *
+ */
+
+public class HomeworkFirst 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("hello world, I have made my first submission on github.");
+
+	}
+
+}
